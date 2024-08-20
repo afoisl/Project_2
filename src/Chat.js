aa8825418f01795a9035ed15ab07266cdacf682a
Sua0764 <imdnasuecho@gmail.com>
2024-08-20 17:14:13 +0900

    채팅룸 UI

--- src/Chat.js
+++ src/Chat.js
@@ -8,37 +8,107 @@ const ChatRoomContainer = styled.div``;
 const MessageList = styled.div`
   background-color: darkgray;
   padding: 20px;
-  border-radius: 5px;
+  border-radius: 10px 10px 0px 0px;
+  display: flex;
+  flex-direction: column;
 `;
 
-const Message = styled.div`
-  padding: 7px;
-  margin: 5px;
+const MessageGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: ${(props) => (props.isOwnMessage ? "flex-end" : "flex-start")};
+  margin-bottom: 10px;
 `;
+
+const MessageContent = styled.div`
+  display: flex;
+  flex-direction: ${(props) => (props.isOwnMessage ? "row-reverse" : "row")};
+  align-items: center;
+`;
+
 const Content = styled.span`
   padding: 7px;
   margin: 5px;
-  background-color: white;
+  background-color: ${(props) => (props.isOwnMessage ? "#DCF8C6" : "white")};
   border-radius: 10px;
+  display: inline-block;
 `;
+
+const Time = styled.span`
+  font-size: 0.8rem;
+  color: #888;
+  margin: ${(props) => (props.isOwnMessage ? "0 10px 0 0" : "0 0 0 10px")};
+`;
+
 const Sender = styled.div`
   padding: 7px;
   margin: 5px;
+  font-weight: bold;
 `;
-const Input = styled.input`
-  height: 30px;
-  width: 200px;
-  margin: 10px 5px 0px 0px;
+
+const MessageSendBox = styled.div`
+  background-color: white;
+  border-radius: 0px 0px 10px 10px;
+  padding: 20px;
+  display: flex;
+`;
+
+const MessageSender = styled.div`
+  box-sizing: border-box;
+  border: 1px solid black;
+  border-radius: 10px;
+  margin: 10px;
+  padding: 20px 0px;
+  width: 90%;
+  display: flex;
+  justify-content: space-between;
+  display: flex;
+  align-items: flex-end;
+  transition: height 0.2s ease;
+`;
+
+const Input = styled.textarea`
+  flex-grow: 1;
+  min-height: 36px;
+  max-height: 150px;
+  padding: 8px;
   border-radius: 5px;
+  resize: none;
+  overflow-y: auto;
+  line-height: 1.5;
+  border: none;
+  outline: none;
+  font-family: inherit;
+  font-size: inherit;
+  background: none;
 `;
+
 const SendBtn = styled.button`
+  background: none;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+
   height: 36px;
   width: 60px;
   border-radius: 5px;
-`;
-const Time = styled.span``;
+  background-color: darkgray;
+  color: white;
 
-const MessageGroup = styled.div``;
+  &:hover {
+    background-color: gray;
+  }
+`;
+
+const MessageAddFile = styled.div`
+  background-color: darkgray;
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+  margin: 10px;
+`;
 
 const Blank = styled.div``;
 
@@ -77,6 +147,17 @@ const formatDate = (date, format = "time") => {
   }
 };
 
+const adjustHeight = (element) => {
+  element.style.height = "auto";
+  element.style.height = element.scrollHeight + "px";
+
+  // MessageSender의 높이도 조절
+  const messageSender = element.closest(".message-sender");
+  if (messageSender) {
+    messageSender.style.height = element.scrollHeight + 16 + "px"; // 16은 패딩 값
+  }
+};
+
 const Chat = ({ userId, roomId }) => {
   const { messages, currentMessage, setCurrentMessage, sendMessage } = useChat(
     userId,
@@ -89,8 +170,11 @@ const Chat = ({ userId, roomId }) => {
   }));
 
   useEffect(() => {
-    console.log("메세지 : ", messages);
-  }, [messages]);
+    const textarea = document.querySelector(".message-sender textarea");
+    if (textarea) {
+      adjustHeight(textarea);
+    }
+  }, [currentMessage]);
 
   const renderMessages = () => {
     let currentDate = null;
@@ -103,12 +187,15 @@ const Chat = ({ userId, roomId }) => {
       const showDateSeparator =
         currentDate === null ||
         (messageDate && messageDate.getTime() !== currentDate.getTime());
+      const isOwnMessage = message.sender === userId;
+      z;
 
       const showSender =
-        index === 0 ||
-        formattedMessages[index - 1].sender !== message.sender ||
-        showDateSeparator ||
-        lastDateSeparator;
+        !isOwnMessage &&
+        (index === 0 ||
+          formattedMessages[index - 1].sender !== message.sender ||
+          showDateSeparator ||
+          lastDateSeparator);
 
       if (showDateSeparator && messageDate) {
         currentDate = messageDate;
@@ -120,13 +207,14 @@ const Chat = ({ userId, roomId }) => {
               <DateText>{formatDate(messageDate, "date")}</DateText>
               <DateLine />
             </DateSeparator>
-            <MessageGroup>
-              <Sender>{message.sender}</Sender>
-              <Blank />
-              <Message>
-                <Content>{message.content}</Content>
-                <Time>{formatDate(message.sentAt)}</Time>
-              </Message>
+            <MessageGroup isOwnMessage={isOwnMessage}>
+              {!isOwnMessage && showSender && <Sender>{message.sender}</Sender>}
+              <MessageContent isOwnMessage={isOwnMessage}>
+                <Content isOwnMessage={isOwnMessage}>{message.content}</Content>
+                <Time isOwnMessage={isOwnMessage}>
+                  {formatDate(message.sentAt)}
+                </Time>
+              </MessageContent>
             </MessageGroup>
           </React.Fragment>
         );
@@ -134,13 +222,14 @@ const Chat = ({ userId, roomId }) => {
 
       lastDateSeparator = false;
       return (
-        <MessageGroup key={`msg-${index}`}>
-          {showSender && <Sender>{message.sender}</Sender>}
-          <Blank />
-          <Message>
-            <Content>{message.content}</Content>
-            <Time>{formatDate(message.sentAt)}</Time>
-          </Message>
+        <MessageGroup key={`msg-${index}`} isOwnMessage={isOwnMessage}>
+          {!isOwnMessage && showSender && <Sender>{message.sender}</Sender>}
+          <MessageContent isOwnMessage={isOwnMessage}>
+            <Content isOwnMessage={isOwnMessage}>{message.content}</Content>
+            <Time isOwnMessage={isOwnMessage}>
+              {formatDate(message.sentAt)}
+            </Time>
+          </MessageContent>
         </MessageGroup>
       );
     });
@@ -149,20 +238,26 @@ const Chat = ({ userId, roomId }) => {
   return (
     <ChatRoomContainer>
       <MessageList>{renderMessages()}</MessageList>
-      <div className="message-input">
-        <Input
-          type="text"
-          value={currentMessage}
-          onChange={(e) => setCurrentMessage(e.target.value)}
-          onKeyPress={(e) => {
-            if (e.key === "Enter") {
-              sendMessage();
-            }
-          }}
-          placeholder="메세지를 입력하세요"
-        />
-        <SendBtn onClick={sendMessage}>Send</SendBtn>
-      </div>
+      <MessageSendBox>
+        <MessageAddFile></MessageAddFile>
+        <MessageSender className="message-sender">
+          <Input
+            value={currentMessage}
+            onChange={(e) => {
+              setCurrentMessage(e.target.value);
+              adjustHeight(e.target);
+            }}
+            onKeyPress={(e) => {
+              if (e.key === "Enter" && !e.shiftKey) {
+                e.preventDefault();
+                sendMessage();
+              }
+            }}
+            placeholder="메세지를 입력하세요"
+          />
+          <SendBtn onClick={sendMessage}>Send</SendBtn>
+        </MessageSender>
+      </MessageSendBox>
     </ChatRoomContainer>
   );
 };
