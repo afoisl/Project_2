df135035ab96436c8b797a69cb80f5d281d364bb
afoisl <160198328+afoisl@users.noreply.github.com>
2024-09-03 16:27:47 +0900

    Customer / Store 수정

--- src/Customer.js
+++ src/Customer.js
@@ -90,16 +90,8 @@ export function Customer() {
 
   const fetchNotices = async () => {
     try {
-      const jwtToken = sessionStorage.getItem("JWT-Token");
-      if (jwtToken != null) {
-        const response = await axios.get("/api/notice", {
-          withCredentials: true,
-          headers: {
-            Authorization: `Bearer ${jwtToken}`,
-          },
-        }); // 백엔드 API 호출
-        setNotices(response.data);
-      } // 가져온 데이터를 상태로 설정
+      const response = await axios.get("http://localhost:8080/api/notice"); // 백엔드 API 호출
+      setNotices(response.data); // 가져온 데이터를 상태로 설정
     } catch (error) {
       console.log("Error fetching notices: ", error);
     }
--- src/Store.js
+++ src/Store.js
@@ -95,17 +95,8 @@ export function Store() {
   const mockTicketStoreItemId = 1; //
 
   useEffect(() => {
-    const jwtToken = sessionStorage.getItem("JWT-Token");
-    if (jwtToken == null) {
-      return;
-    }
     axios
-      .get(`http://localhost:8080/api/storeItem/${bookStoreItemId}/books`, {
-        withCredentials: true,
-        headers: {
-          Authorization: `Bearer ${jwtToken}`,
-        },
-      })
+      .get(`http://localhost:8080/api/storeItem/${bookStoreItemId}/books`)
       .then((response) => {
         console.log("Books API Response:", response.data);
         setBooks(response.data);
@@ -116,13 +107,7 @@ export function Store() {
 
     axios
       .get(
-        `http://localhost:8080/api/storeItem/${mockTicketStoreItemId}/mockTickets`,
-        {
-          withCredentials: true,
-          headers: {
-            Authorization: `Bearer ${jwtToken}`,
-          },
-        }
+        `http://localhost:8080/api/storeItem/${mockTicketStoreItemId}/mockTickets`
       )
       .then((response) => {
         console.log("MockTickets API Response:", response.data);
