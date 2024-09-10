ebd27e2067ffed18fdb03eb6850c7678f235edaa
브니 <161662792+DaramgeOvO@users.noreply.github.com>
2024-09-10 09:28:34 +0900

    import 지우기

--- src/QnaList.js
+++ src/QnaList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import style from "style-component";
+
 import styled from "styled-components";
 
 const ContList = styled.div`
