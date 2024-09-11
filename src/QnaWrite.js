3eb86b5a438e12fa2e73221fb56c4d0658992929
afoisl <160198328+afoisl@users.noreply.github.com>
2024-09-11 18:09:56 +0900

    수정

--- src/QnaWrite.js
+++ src/QnaWrite.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
